66ea670e671d265686374c183f22aec6bd399460
breezjirasak <jirasak.tu@ku.th>
2022-09-13 19:33:25 +0700

    edit end_date model.

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('ended date', null=True, default=None)
+    end_date = models.DateTimeField('ended date', null=True, blank=True)
     
     def __str__(self):
         return self.question_text
