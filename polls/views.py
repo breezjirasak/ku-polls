a016f195e41c7f29cb8327f4fa127b0526a1cbe7
breezjirasak <jirasak.tu@ku.th>
2022-09-13 19:35:40 +0700

    closes #12; show the error message if voting is not allowed.

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Question, Choice
 
@@ -29,6 +30,14 @@ class DetailView(generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    def get(self, request, pk):
+        self.question = Question.objects.get(pk=pk)
+        if self.question.can_vote():
+            return render(request, 'polls/detail.html', {'question': self.question})
+        else:
+            messages.error(request, "Voting is not allowed.")
+            return HttpResponseRedirect(reverse('polls:index'))
 
 
 class ResultsView(generic.DetailView):
